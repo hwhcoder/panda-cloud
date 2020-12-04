b1fd4a7fbea922db41c5a0b8b4ed49949ba30b31
weihuang.hua <weihuang.hua@ucarinc.com>
2020-12-04 18:29:13 +0800

    1

--- panda-config-server/src/main/java/com/yukong/panda/config/server/PandaConfigServerApplication.java
+++ panda-config-server/src/main/java/com/yukong/panda/config/server/PandaConfigServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 本工程比较简单，没有业务逻辑
+ */
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
--- panda-server/src/main/java/com/yukong/panda/server/PandaServerApplication.java
+++ panda-server/src/main/java/com/yukong/panda/server/PandaServerApplication.java
@@ -2,10 +2,12 @@ package com.yukong.panda.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//Eureka服务端
+/**
+ * Eureka服务端
+ * 本工程比较简单，没有业务逻辑
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class PandaServerApplication {
